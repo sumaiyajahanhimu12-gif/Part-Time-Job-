cedd2ad0a173edc6f9eaf8955c72b1f8e69690d5
sumaiyajahanhimu12-gif <sumaiyajahanhimu12@gmail.com>
2026-08-04 23:15:38 +0600

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,19 @@
 import { db } from "./firebase.js";
 
-alert("Firebase Connected Successfully");
+const tg = window.Telegram?.WebApp;
 
-console.log(db);
+if (tg) {
+    tg.expand();
+
+    console.log("Telegram User:", tg.initDataUnsafe?.user);
+
+    document.querySelector(".loading").innerHTML = `
+        <h1>💼 Part Time Job</h1>
+        <p>Welcome ${tg.initDataUnsafe?.user?.first_name || "User"}</p>
+    `;
+} else {
+    document.querySelector(".loading").innerHTML = `
+        <h1>💼 Part Time Job</h1>
+        <p>Open inside Telegram</p>
+    `;
+}

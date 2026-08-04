9f940261275d5ad6ef2d9490f094b20347cf17af
sumaiyajahanhimu12-gif <sumaiyajahanhimu12@gmail.com>
2026-08-04 23:02:56 +0600

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,5 +1,4 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
 const firebaseConfig = {

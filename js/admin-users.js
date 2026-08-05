e0487f40ca95620a384734e0a3ef6edbb379b84e
sumaiyajahanhimu12-gif <sumaiyajahanhimu12@gmail.com>
2026-08-05 23:41:37 +0600

    Update admin-users.js

--- js/admin-users.js
+++ js/admin-users.js
@@ -1,1 +1,65 @@
+import { db } from "./firebase.js";
 
+import {
+  collection,
+  getDocs
+}
+from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
+loadUsers();
+
+async function loadUsers() {
+
+  const snap =
+    await getDocs(
+      collection(db,"users")
+    );
+
+  let html = "";
+
+  snap.forEach(user => {
+
+    const data =
+      user.data();
+
+    html += `
+
+    <div class="task-card">
+
+    <h3>
+    ${data.firstName || ""}
+    </h3>
+
+    <p>
+    Telegram ID:
+    ${data.telegramId}
+    </p>
+
+    <p>
+    Coins:
+    ${data.coin || 0}
+    </p>
+
+    <p>
+    Status:
+    ${data.status}
+    </p>
+
+    <a
+    href="${data.facebookLink || "#"}"
+    target="_blank"
+    >
+    Facebook Profile
+    </a>
+
+    </div>
+
+    `;
+
+  });
+
+  document.getElementById(
+    "usersContainer"
+  ).innerHTML = html;
+
+}

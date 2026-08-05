235ef21ac827830b6fa0c4df0f4290f8c35e4160
sumaiyajahanhimu12-gif <sumaiyajahanhimu12@gmail.com>
2026-08-05 23:42:25 +0600

    Update admin-tasks.js

--- js/admin-tasks.js
+++ js/admin-tasks.js
@@ -1,1 +1,58 @@
+import { db } from "./firebase.js";
 
+import {
+  collection,
+  getDocs
+}
+from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
+loadTasks();
+
+async function loadTasks() {
+
+  const snap =
+    await getDocs(
+      collection(db,"tasks")
+    );
+
+  let html = "";
+
+  snap.forEach(task => {
+
+    const data =
+      task.data();
+
+    html += `
+
+    <div class="task-card">
+
+    <h3>
+    ${data.name}
+    </h3>
+
+    <p>
+    Reward:
+    ${data.coin}
+    </p>
+
+    <p>
+    Type:
+    ${data.taskType}
+    </p>
+
+    <p>
+    Completed:
+    ${data.completedCount || 0}
+    </p>
+
+    </div>
+
+    `;
+
+  });
+
+  document.getElementById(
+    "tasksContainer"
+  ).innerHTML = html;
+
+}

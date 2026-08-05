c80db7e3e6b881eea14b560b60ba28863b6bd733
sumaiyajahanhimu12-gif <sumaiyajahanhimu12@gmail.com>
2026-08-05 23:54:10 +0600

    Update admin-settings.js

--- js/admin-settings.js
+++ js/admin-settings.js
@@ -1,1 +1,104 @@
+import { db } from "./firebase.js";
 
+import {
+  doc,
+  getDoc,
+  setDoc
+}
+from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
+loadSettings();
+
+async function loadSettings() {
+
+  const settingsRef =
+    doc(db, "settings", "system");
+
+  const snap =
+    await getDoc(settingsRef);
+
+  if (!snap.exists()) {
+    return;
+  }
+
+  const data =
+    snap.data();
+
+  document.getElementById(
+    "withdrawEnabled"
+  ).value =
+    String(data.withdrawEnabled);
+
+  document.getElementById(
+    "minWithdraw"
+  ).value =
+    data.minWithdraw || 50000;
+
+  document.getElementById(
+    "referralPercent"
+  ).value =
+    data.referralPercent || 5;
+
+  document.getElementById(
+    "taskCooldown"
+  ).value =
+    data.taskCooldown || 24;
+
+}
+
+document
+.getElementById(
+  "saveSettingsBtn"
+)
+.addEventListener(
+  "click",
+  saveSettings
+);
+
+async function saveSettings() {
+
+  const withdrawEnabled =
+    document.getElementById(
+      "withdrawEnabled"
+    ).value === "true";
+
+  const minWithdraw =
+    Number(
+      document.getElementById(
+        "minWithdraw"
+      ).value
+    );
+
+  const referralPercent =
+    Number(
+      document.getElementById(
+        "referralPercent"
+      ).value
+    );
+
+  const taskCooldown =
+    Number(
+      document.getElementById(
+        "taskCooldown"
+      ).value
+    );
+
+  await setDoc(
+    doc(
+      db,
+      "settings",
+      "system"
+    ),
+    {
+      withdrawEnabled,
+      minWithdraw,
+      referralPercent,
+      taskCooldown
+    }
+  );
+
+  alert(
+    "Settings Saved Successfully"
+  );
+
+}
